0662641c563910ce8ec4f418ac5871a66ae8fd8a
laurenmnaylor <145718783+laurenmnaylor@users.noreply.github.com>
2023-09-26 16:10:25 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,3 @@
-# The configuration for the `remote` backend.
-terraform {
-  backend "remote" {
-    # The name of your Terraform Cloud organization.
-    organization = "example-organization"
-
-    # The name of the Terraform Cloud workspace to store Terraform state files in.
-    workspaces {
-      name = "example-workspace"
-    }
-  }
-}
-
 resource "okta_app_oauth" "test_app" {
   label                      = "Terraform Test SPA"
   type                       = "browser"
