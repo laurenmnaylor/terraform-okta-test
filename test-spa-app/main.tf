577801c0371041c6dd43e952191a5dc6fe81cbf1
laurenmnaylor <145718783+laurenmnaylor@users.noreply.github.com>
2023-10-18 17:02:20 -0500

    Update main.tf

--- test-spa-app/main.tf
+++ test-spa-app/main.tf
@@ -2,7 +2,7 @@ resource "okta_app_oauth" "test_app" {
   label                      = "Test SPA App 1"
   type                       = "browser"
   grant_types                = ["authorization_code"]
-  redirect_uris              = vars.login_redirect_uri
+  redirect_uris              = ["https://example.com"]
   consent_method             = "REQUIRED"
   token_endpoint_auth_method = "none"
   response_types             = ["code"]
