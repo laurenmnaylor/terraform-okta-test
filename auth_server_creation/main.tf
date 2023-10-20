2e45579fb8f4db8b42a46aa313a78869a6d4ce0f
laurenmnaylor <145718783+laurenmnaylor@users.noreply.github.com>
2023-10-20 09:44:52 -0500

    Update and rename auth_servers.tf to main.tf

---
+++ auth_server_creation/main.tf
@@ -0,0 +1,20 @@
+terraform {
+       backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "terraform-okta-test"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "auth-servers"
+         }
+       }
+     }
+
+
+resource "okta_auth_server" "my_auth_server" {
+  audiences   = ["api://example"]
+  description = "My Example Auth Server"
+  name        = "My Auth Server"
+  issuer_mode = "ORG_URL"
+  status      = "ACTIVE"
+}
