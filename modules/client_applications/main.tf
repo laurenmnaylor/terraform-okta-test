ba9d8c9c182e7641961d6b026fbc827eb56cdc60
Lauren Naylor <145718783+laurenmnaylor@users.noreply.github.com>
2023-11-14 14:55:56 -0600

    Update main.tf

--- modules/client_applications/main.tf
+++ modules/client_applications/main.tf
@@ -1,5 +1,5 @@
 resource "okta_app_oauth" "terraform_client" {
-  for_each = setintersection(toset(tolist(var.env)), toset(var.envs_to_deploy))
+  for_each = setintersection(toset([var.env]), toset(var.envs_to_deploy))
   label                      = each.key
   type                       = "service"
   grant_types                = ["client_credentials"]
