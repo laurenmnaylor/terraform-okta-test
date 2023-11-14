6b3103bee4916d9e7aea4db0b3b030fbfce0646f
Lauren Naylor <145718783+laurenmnaylor@users.noreply.github.com>
2023-11-14 14:16:58 -0600

    Update main.tf

--- test-spa-app/main.tf
+++ test-spa-app/main.tf
@@ -11,4 +11,7 @@ resource "okta_app_oauth" "test_app" {
 
 module "client_module" {
   source = "../modules/client_applications"
+  org_name = var.org_name
+  api_token = var.api_token
+  base_url = var.base_url
 }
