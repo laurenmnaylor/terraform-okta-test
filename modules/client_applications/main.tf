299d61e18a4b9f3692a4696fba0458c7c5a4ada3
Lauren Naylor <145718783+laurenmnaylor@users.noreply.github.com>
2023-11-14 14:37:12 -0600

    Update main.tf

--- modules/client_applications/main.tf
+++ modules/client_applications/main.tf
@@ -1,5 +1,5 @@
 resource "okta_app_oauth" "terraform_client" {
-  for_each = toset(["Test Module Client"])
+  for_each = []
   label                      = each.key
   type                       = "service"
   grant_types                = ["client_credentials"]
