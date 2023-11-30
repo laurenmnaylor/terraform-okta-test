00d9005ebce6007cbb719852149a5e6146b40009
Lauren Naylor <145718783+laurenmnaylor@users.noreply.github.com>
2023-11-30 13:28:10 -0600

    Update main.tf

--- test-spa-app/main.tf
+++ test-spa-app/main.tf
@@ -8,10 +8,10 @@ terraform {
            name = "test-spa-app"
          }
        }
-     }
+}
 
 module "client_module" {
-  source = "../modules/client_applications"
+  source = "/modules/client_applications"
   org_name = var.org_name
   api_token = var.api_token
   base_url = var.base_url
