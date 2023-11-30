441d7a81f4b310d72a4d014f85cf6e71506268b2
Lauren Naylor <145718783+laurenmnaylor@users.noreply.github.com>
2023-11-30 16:11:31 -0600

    Update other_scopes.tf

--- other_scopes.tf
+++ other_scopes.tf
@@ -1,10 +1,1 @@
-data "okta_auth_server" "as" {
-   name = "example"
-}
 
-resource "okta_auth_server_scope" "example_scope_2" {
-  auth_server_id   = data.okta_auth_server.as.id
-  metadata_publish = "NO_CLIENTS"
-  name             = "exampleNumberTwo"
-  consent          = "IMPLICIT"
-}
