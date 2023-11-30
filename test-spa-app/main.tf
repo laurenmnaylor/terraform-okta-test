7b31d7221e4571a95005aa7f02e0a7ea4fe5ebf4
Lauren Naylor <145718783+laurenmnaylor@users.noreply.github.com>
2023-11-30 13:34:04 -0600

    Update main.tf

--- test-spa-app/main.tf
+++ test-spa-app/main.tf
@@ -10,11 +10,22 @@ terraform {
        }
 }
 
-module "client_module" {
+resource "okta_app_oauth" "test_app" {
+  label                      = "Test SPA App 3"
+  type                       = "browser"
+  grant_types                = ["authorization_code"]
+  redirect_uris              = ["https://example.com"]
+  consent_method             = "REQUIRED"
+  token_endpoint_auth_method = "none"
+  response_types             = ["code"]
+  post_logout_redirect_uris  = ["https://example.com", "https://example2.com"]
+}
+
+/**module "client_module" {
   source = "../modules/client_applications"
   org_name = var.org_name
   api_token = var.api_token
   base_url = var.base_url
   env = var.env
   envs_to_deploy = ["dev","qa"]
-}
+}**/
