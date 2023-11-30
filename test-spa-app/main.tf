9f3ef87028e2ea5bd8d9d529851b1192e2d8bdfa
Lauren Naylor <145718783+laurenmnaylor@users.noreply.github.com>
2023-11-30 13:19:40 -0600

    Update main.tf

--- test-spa-app/main.tf
+++ test-spa-app/main.tf
@@ -1,13 +1,14 @@
-resource "okta_app_oauth" "test_app" {
-  label                      = "Test SPA App 3"
-  type                       = "browser"
-  grant_types                = ["authorization_code"]
-  redirect_uris              = ["https://example.com"]
-  consent_method             = "REQUIRED"
-  token_endpoint_auth_method = "none"
-  response_types             = ["code"]
-  post_logout_redirect_uris  = ["https://example.com", "https://example2.com"]
-}
+terraform {
+       backend "remote" {
+         # The name of your Terraform Cloud organization.
+         organization = "terraform-okta-test"
+
+         # The name of the Terraform Cloud workspace to store Terraform state files in.
+         workspaces {
+           name = "test-spa-app"
+         }
+       }
+     }
 
 module "client_module" {
   source = "../modules/client_applications"
