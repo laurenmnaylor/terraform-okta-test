d24008d6507d633cf6d67cee9b130a8241b0e2c8
Lauren Naylor <lnaylor@paraport.com>
2024-01-09 09:23:27 -0600

    use double quotes

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "okta" {
-  org_name = 'dev-86564089'
-  base_url = 'okta.com'
-  api_token = '00qbbg0HkcvtT7DuiFOzRhBlk0WmE3Rmc_Sfj8MVb8'
+  org_name = "dev-86564089"
+  base_url = "okta.com"
+  api_token = "00qbbg0HkcvtT7DuiFOzRhBlk0WmE3Rmc_Sfj8MVb8"
 }
