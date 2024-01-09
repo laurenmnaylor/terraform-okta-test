94547e236210bcea3620d05ec209766723f04b9e
Lauren Naylor <lnaylor@paraport.com>
2024-01-09 11:31:15 -0600

    debug

--- vars.tf
+++ vars.tf
@@ -2,24 +2,13 @@ variable "org_name" {
   type = string
 }
 
-output "org_value" {
-  value = var.org_name
-}
-
 variable "api_token" {
   type = string
 }
 
-output "t_value" {
-  value = nonsensitive(var.api_token)
-}
 
 variable "base_url" {
   type = string
-}
-
-output "url_value" {
-  value = var.base_url
 }
 
 variable "env" {
