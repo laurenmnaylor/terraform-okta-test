b450bc3dcd09053d61780ca9f7901caacced2a56
Lauren Naylor <lnaylor@paraport.com>
2024-01-10 14:21:58 -0600

    remove app

--- main.tf
+++ main.tf
@@ -10,13 +10,13 @@
        }
      }
 
-resource "okta_app_oauth" "test_app" {
-  label                      = "Terraform Test SPA"
-  type                       = "browser"
-  grant_types                = ["authorization_code"]
-  redirect_uris              = ["https://example.com/login/authorization_code/callback"]
-  consent_method             = "REQUIRED"
-  token_endpoint_auth_method = "none"
-  response_types             = ["code"]
-  post_logout_redirect_uris  = ["https://example.com", "https://example2.com"]
-}
+#resource "okta_app_oauth" "test_app" {
+#  label                      = "Terraform Test SPA"
+#  type                       = "browser"
+#  grant_types                = ["authorization_code"]
+#  redirect_uris              = ["https://example.com/login/authorization_code/callback"]
+#  consent_method             = "REQUIRED"
+#  token_endpoint_auth_method = "none"
+#  response_types             = ["code"]
+#  post_logout_redirect_uris  = ["https://example.com", "https://example2.com"]
+#}
