62d4441f702ae62ffb0f65cc84f0caffe5d0da59
Lauren Naylor <lnaylor@paraport.com>
2024-02-26 12:16:31 -0600

    comment out backend remote config

--- provider.tf
+++ provider.tf
@@ -3,5 +3,5 @@ provider "okta" {
   base_url = var.base_url
   # Terraform cloud is messing with pipeline's ability to pass token value. 
   # Hardcoding here because so I can test other things and my personal Okta account doesn't control anything and is just for testing terraform
-  api_token = "00qbbg0HkcvtT7DuiFOzRhBlk0WmE3Rmc_Sfj8MVb8"
+  api_token = "00zwU-q7eTVKizfbDVEPzXcVKqHyAJIXWjNY5OaOit"
 }
