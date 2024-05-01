c3c5d2000b53d11dbf2f5576c220b2ff04dc49f6
Lauren Naylor <lnaylor@paraport.com>
2024-05-01 14:33:31 -0500

    auth server

--- main.tf
+++ main.tf
@@ -15,16 +15,16 @@ resource "okta_auth_server" "authorization_server" {
   name      = "My Authorization Server"
 }
 
-resource "okta_app_oauth" "test_app" {
-  label                      = "Terraform Test SPA"
-  type                       = "browser"
-  grant_types                = ["authorization_code"]
-  redirect_uris              = ["https://example.com/login/authorization_code/callback"]
-  consent_method             = "REQUIRED"
-  token_endpoint_auth_method = "none"
-  response_types             = ["code"]
-  post_logout_redirect_uris  = ["https://example.com", "https://example2.com"]
-}
+#resource "okta_app_oauth" "test_app" {
+#  label                      = "Terraform Test SPA"
+#  type                       = "browser"
+#  grant_types                = ["authorization_code"]
+#  redirect_uris              = ["https://example.com/login/authorization_code/callback"]
+#  consent_method             = "REQUIRED"
+#  token_endpoint_auth_method = "none"
+#  response_types             = ["code"]
+#  post_logout_redirect_uris  = ["https://example.com", "https://example2.com"]
+#}
 # 
 # resource "okta_app_user_schema_property" "custom_app_profile" {
 #   app_id = okta_app_oauth.test_app.id
