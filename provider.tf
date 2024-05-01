ada726ad6f5496f515f02fb43b0ab64a2935d9f0
Lauren Naylor <lnaylor@paraport.com>
2024-05-01 15:29:19 -0500

    add scopes

--- provider.tf
+++ provider.tf
@@ -3,7 +3,7 @@ provider "okta" {
   base_url = var.base_url
   client_id = "0oafdk1fs2p3S2ZmD5d7"
   private_key = var.api_token
-  scopes = ["okta.authorizationServers.manage", "okta.authorizationServers.read", "okta.apps.manage", "okta.apps.read"]
+  scopes = ["okta.authorizationServers.manage", "okta.authorizationServers.read", "okta.apps.manage", "okta.apps.read", "okta.policies.read"]
   # Terraform cloud is messing with pipeline's ability to pass token value. 
   # Hardcoding here because so I can test other things and my personal Okta account doesn't control anything and is just for testing terraform
 #   api_token = "003Fzxrtmlf6zBuasEy_BEqhbaKjyNqPcM2sUGXuKZ"
