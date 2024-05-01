f4e41bd58803dc996c627ebb0b46b4937dd6a59c
Lauren Naylor <lnaylor@paraport.com>
2024-05-01 16:12:38 -0500

    add group scopes

--- provider.tf
+++ provider.tf
@@ -4,7 +4,7 @@ provider "okta" {
   client_id = "0oafdk1fs2p3S2ZmD5d7"
   private_key = var.api_token
   scopes = ["okta.authorizationServers.manage", "okta.authorizationServers.read", "okta.apps.manage", "okta.apps.read", "okta.policies.read", "okta.schemas.manage", "okta.schemas.read",
-  "okta.profileMappings.manage", "okta.profileMappings.read"]
+  "okta.profileMappings.manage", "okta.profileMappings.read", "okta.groups.read", "okta.groups.manage"]
   # Terraform cloud is messing with pipeline's ability to pass token value. 
   # Hardcoding here because so I can test other things and my personal Okta account doesn't control anything and is just for testing terraform
 #   api_token = "003Fzxrtmlf6zBuasEy_BEqhbaKjyNqPcM2sUGXuKZ"
