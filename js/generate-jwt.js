a57845dc5aac1183f3372248b9f9aea623057ef2
Lauren Naylor <lnaylor@paraport.com>
2024-05-22 10:32:57 -0500

    generate jwt

--- js/generate-jwt.js
+++ js/generate-jwt.js
@@ -12,7 +12,7 @@ const clientId = process.argv[3];
 
 const now = Math.floor( new Date().getTime() / 1000 ); // seconds since epoch
 const plus5Minutes = new Date( ( now + (5*60) ) * 1000); // Date object
-const alg = 'RS256'; // one of RSA or ECDSA algorithms: RS256, RS384, RS512, ES256, ES384, ES512
+const alg = 'RS384'; // one of RSA or ECDSA algorithms: RS256, RS384, RS512, ES256, ES384, ES512
 
 const url = process.argv[4];
 const claims = {
