e0c802ab48ed30f37c533eca62e8e183f0b93786
Lauren Naylor <lnaylor@paraport.com>
2024-05-28 14:30:40 -0500

    test data

--- other.tf
+++ other.tf
@@ -1,16 +1,16 @@
-# data "okta_app_oauth" "test_app" {
-#     label = "Terraform Test SPA 3"
-# }
-# 
-# resource "okta_auth_server" "authorization_server" {
-#  audiences = ["my_aud"]
-#  name      = "My Authorization Server"
-# }
-# 
-# resource "okta_auth_server_policy" "auth_server_policy" {
-#  auth_server_id = okta_auth_server.authorization_server.id
-#  name = "My Policy"
-#  priority = 1
-#  description = "My Policy Description"
-#  client_whitelist = [data.okta_app_oauth.test_app.id]
-# }
+data "okta_app_oauth" "test_app" {
+    label = "Terraform Test SPA 3"
+}
+
+resource "okta_auth_server" "authorization_server" {
+ audiences = ["my_aud"]
+ name      = "My Authorization Server"
+}
+
+resource "okta_auth_server_policy" "auth_server_policy" {
+ auth_server_id = okta_auth_server.authorization_server.id
+ name = "My Policy"
+ priority = 1
+ description = "My Policy Description"
+ client_whitelist = [data.okta_app_oauth.test_app.id]
+}
